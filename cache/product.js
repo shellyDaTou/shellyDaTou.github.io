c4def963d0d4db95f3de3c8deea18ca056480894
datou <337414932@qq.com>
2018-08-21 22:50:02 +0800

    【产品中心】处理不同类目商品的展示

---
+++ cache/product.js
@@ -0,0 +1,121 @@
+$(function(){
+    //    初始化
+    $('#showLayerImg').hide();
+   // 初始化显示所有的图片
+   jointLiStr(productArr);
+  })
+
+//   给动态添加的li绑定点击事件
+  $('#showProductUl').on('click','li',function() {
+    //   弹框
+      var layerImg =  $('#showLayerImg');
+    //   li内的img的src
+      var src = $(this).find('img').attr('src');
+      layerImg.find('img').attr('src', src);
+      layer.open({
+          type: 1,
+          title: false,
+          area: ['60%', '60%'],
+          content: layerImg,
+          cancel: function() {
+              layerImg.hide();
+          }
+        });
+
+  })
+
+//   产品路径、类型、名称
+  let productArr = [{
+      url: 'upload/1505356879.jpg',
+      type: 'bed',
+      name: '床1'
+  },
+  {
+      url: 'upload/1505356890.jpg',
+      type: 'desk',
+      name: '桌1'
+  },
+  {
+      url: 'upload/1505356924.jpg',
+      type: 'chair',
+      name: '椅1'
+  },
+  {
+      url: 'upload/1505356986.jpg',
+      type: 'cabinet',
+      name: '柜1'
+  },
+  {
+      url: 'upload/1505374754black.jpg',
+      type: 'sofa',
+      name: '沙发1'
+  },
+  {
+      url: 'upload/1505379305.jpg',
+      type: 'cabinet',
+      name: '柜2'
+  },{
+      url: 'upload/1505381827.jpg',
+      type: 'bed',
+      name: '床2'
+  }];
+
+  // 产品中心点页面击不同类目展示不同类商品
+  $('.kind-list li').on('click', function() {
+      $('#showProductUl').empty();
+      const str = $(this).attr('class');
+      filterLiArr(str);
+      // TODO 添加点击后的样式
+      // $(this).addClass('visite-kind-list')
+      //        .siblings()
+      //        .removeClass('visite-kind-list');
+  })
+
+  // 展示图片
+  function filterLiArr(value= 'all') {
+      let str = '';
+      let newArr = [];
+      if (value === all ) {
+          // 直接拼接所以数组项
+          jointLiStr(productArr);
+          return;
+      }
+
+      productArr.filter((item) => {
+          if (item.type === value) {
+              newArr.push(item);
+          }
+          })
+       jointLiStr(newArr);
+  }
+
+//   拼接不同类型的产品li
+  function jointLiStr(arr) {
+      let str = '';
+       arr.forEach((item) => {
+              str += `<li class="shown">
+              <div class="widget widget-shadow">
+                  <figure class="widget-header cover showLayer">
+                      <a title="${item['name']}"
+                          target='_self'>
+                          <div class="mask">
+                          </div>
+                          <img class="cover-image"
+                              src="${item['url']}"
+                              alt="${item['name']}"
+                              style='height:200px;'>
+                      </a>
+                  </figure>
+                  <h4 class="widget-title">
+                      <a title="${item['name']}"
+                          target='_self'>${item['name']}</a>
+                      <p></p>
+                  </h4>
+              </div>
+              </li>`
+      });
+      $('#showProductUl').append(str);
+      }
+
+
+
